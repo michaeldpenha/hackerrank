b7682d9404e379767c493e5637893b2e47ee55eb
Michael Dpenha <michael.dpenha@intive.com>
2018-11-23 12:16:31 +0100

    Array reduction algorithim

---
+++ arrayreduction/arrayreduction.js
@@ -0,0 +1,22 @@
+arrayReduction = (arrayOfNum) => {
+    // we need to get all the unique numbers from an array and sort it
+    let uniqueNumbersFromAnArray =  [... new Set(arrayOfNum.sort((a ,b)=>{return a-b}))];
+    let additionOfNumbers = [];
+
+    while (uniqueNumbersFromAnArray.length > 1){
+        const num1 = uniqueNumbersFromAnArray[0];
+        const num2 = uniqueNumbersFromAnArray[1];
+        const total = num1 + num2;
+        
+        additionOfNumbers.push(total);
+
+        uniqueNumbersFromAnArray.splice(0,2,total); // splice(startIndex, endIndex , replaceValue)
+        uniqueNumbersFromAnArray = uniqueNumbersFromAnArray.sort((a,b)=>{return a-b});
+    }
+
+    return additionOfNumbers.reduce((a,b) => {return a+b});
+}
+
+// [3,1,2,3] > 9
+// [1,2,3,4] > 3 6 10  > 19
+// [23,4,5,62,22,1,2,3] > [1,2,3,4,5,22,23,62] > 3 6 9 15 37 60 122 >  
